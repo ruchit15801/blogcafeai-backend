04f0ca67afa04fb6f7a31bc9b5986b8f503025ae
ruchit15801 <ruchit.15801@gmail.com>
2025-09-19 19:23:40 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Security & parsing
 app.use(helmet());
-app.use(cors({ origin: process.env.FRONTEND_URL || '*', credentials: true }));
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
